099b92d4548e71525763042bd04e1209d3cc1961
Luca Fiore <lf44551@gmail.com>
2026-02-06 14:38:03 +0100

    Aggiungi route per visualizzare il dettaglio dei post

--- routers/posts.js
+++ routers/posts.js
@@ -1,4 +1,9 @@
 // index
 app.get('/posts', function (req, res) {
-    res.send('Lista dei post')
+    res.send('Lista dei post');
 });
+
+// show
+app.get('/posts/:id', function (req, res) {
+    res.send('Dettaglio dei post' + req.params.id);
+}); 
