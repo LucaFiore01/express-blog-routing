47af12a296eff774e1eeb578f23b94b2d8af0e3a
Luca Fiore <lf44551@gmail.com>
2026-02-06 14:47:17 +0100

    Aggiungi router per gestire le route dei post

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const postsRouter = require('./routers/posts');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
 });
+
+app.use('/posts', postsRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
